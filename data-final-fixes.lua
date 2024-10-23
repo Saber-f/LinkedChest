8fadd665f792398004c19f11bb5735fce41d2b2a
dayin <735974175@qq.com>
2024-10-24 03:23:25 +0800

    1、关联箱支持设置筛选

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -5,3 +5,8 @@ data.raw["recipe"]["barrel"].ingredients = {
 
 local OemLinkedChest = util.table.deepcopy(data.raw["linked-container"]["linked-chest"])
 OemLinkedChest.gui_mode = "all" -- all, none, adminss
+
+
+for index, container in pairs(data.raw["linked-container"]) do
+    container.inventory_type = "with_filters_and_bar"
+end
