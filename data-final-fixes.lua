a96abffb7a432c93d9fa31eb8b550295061c72e6
dayin <735974175@qq.com>
2024-12-26 21:16:50 +0800

    调整雷达范围和机器人平台范围

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -13,3 +13,10 @@ end
 
 -- 碰撞0.4
 data.raw["linked-container"]["Oem-linked-chest"].collision_box = {{-0.15, -0.15}, {0.15, 0.15}}
+
+-- 机器人平台雷达范围
+data.raw.roboport.roboport.radar_range = 2
+data.raw.roboport.roboport.logistics_radius = 64
+data.raw.roboport.roboport.construction_radius = 64
+data.raw.roboport.roboport.logistics_connection_distance = 64
+data.raw["cargo-landing-pad"]["cargo-landing-pad"].radar_range = 2
